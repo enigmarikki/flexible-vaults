89c3cda21b686c1fddfae82905e8329ec9bfd2c4
enigmarikki <hrishi98.m@gmail.com>
2026-01-28 17:00:38 -0500

    "chore: bump sv3"

--- scripts/ethereum/GenerateAaveOpsJSON.s.sol
+++ scripts/ethereum/GenerateAaveOpsJSON.s.sol
@@ -436,11 +436,13 @@ contract GenerateAaveOpsJSON is Script, Test {
         );
     }
 
-    /// @notice Generate ALL sv3 ops (Aave + Spark with both eMode 0 and 32)
+    /// @notice Generate ALL sv3 ops (Aave + Spark with eMode 0, 1, and 32)
     function generatePreProdSv3All() public {
         generatePreProdSv3Aave(0);
+        generatePreProdSv3Aave(1);
         generatePreProdSv3Aave(32);
         generatePreProdSv3Spark(0);
+        generatePreProdSv3Spark(1);
         generatePreProdSv3Spark(32);
     }
 
